0d6e40029c59972e89bd390ea05d61e614db2bb5
syed Riyaz <76534599+riyazulahad786@users.noreply.github.com>
2023-10-14 14:39:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import StockTracker from './Components/StockTracker'
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <StockTracker/>
+      
+    </div>
+  )
+}
+
+export default App
+
